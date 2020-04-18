97ebbf2a3a65bd433d2632cd03eaa6509c3d226b
Michael Dorst <m@mdorst.net>
2020-04-18 15:28:15 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('express')()
+
+mongoose
+  .connect('mongodb://localhost:27017/kingdom', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to database.')
+    // Send out an event that can be listened to elsewhere
+    app.emit('event:database-connected')
+    app.set('database-connected', true)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
